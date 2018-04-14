5e360edc270a6fe7b3022a526c1bc6b02b0b48ad
Augusto Goulart <augusto@goulart.me>
2018-04-14 17:26:52 -0300

    ex1

--- ex1/augusto_ex1.c
+++ ex1/augusto_ex1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/* This is a comment. */
+/* This is a comment */
 int main(int argc, char *argv[])
 {
     int distance = 100;
 
-    // this is also a comment
+    // this is another comment
     printf("You are %d miles away.\n", distance);
 
     return 0;
